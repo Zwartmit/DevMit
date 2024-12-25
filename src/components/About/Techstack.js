0ade3293d7721309d8c2d7d9cad5f4c4f96dce8e
Zwartmit <brandonurbanobuz@gmail.com>
2024-12-24 19:30:47 -0500

    v3

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Col, Row } from "react-bootstrap";
-import { DiJavascript1, DiReact, DiPython, DiGit, DiDjango } from "react-icons/di";
+import { DiJavascript1, DiReact, DiPython, DiDjango } from "react-icons/di";
 import { FaCss3Alt, FaHtml5 } from "react-icons/fa";
 
 function Techstack() {
