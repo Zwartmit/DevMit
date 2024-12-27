0fa3099872f355dfe5d44e4dff154a8924bec9cb
Zwartmit <brandonurbanobuz@gmail.com>
2024-12-27 00:17:16 -0500

    Modales en ProjectCards

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -1,31 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
-import { BsGithub } from "react-icons/bs";
+import Modal from "react-bootstrap/Modal";
+import Carousel from "react-bootstrap/Carousel";
+import { BsGithub, BsSearch } from "react-icons/bs";
 
 function ProjectCards(props) {
-  return (
-    <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
-      <Card.Body>
-        <Card.Title style={{ fontWeight: "bold" }}>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}></Card.Text>
-        <Card.Text style={{ textAlign: "center" }}>{props.description}</Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
-        {"\n"}
-        {"\n"}
+  const [show, setShow] = useState(false);
 
-        {/* {!props.isBlog && props.demoLink && (
-          <Button variant="primary" href={props.demoLink} target="_blank" style={{ marginLeft: "10px" }}>
-            <CgWebsite /> &nbsp;
-            {"Demo"}
-          </Button>
-        )} */}
-      </Card.Body>
-    </Card>
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+
+  return (
+    <>
+      <Card className="project-card-view">
+        <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+        <Card.Body>
+          <Card.Title style={{ fontWeight: "bold" }}>{props.title}</Card.Title>
+          <Card.Text style={{ textAlign: "center" }}>{props.description}</Card.Text>
+          <div style={{ display: "flex", justifyContent: "center" }}>
+            <Button variant="primary" href={props.ghLink} target="_blank">
+              <BsGithub /> &nbsp;
+              {props.isBlog ? "Blog" : "GitHub"}
+            </Button>&nbsp;&nbsp;
+            <Button variant="secondary" onClick={handleShow}>
+              <BsSearch />
+            </Button>
+          </div>
+        </Card.Body>
+      </Card>
+
+      <Modal show={show} onHide={handleClose} centered>
+        <Modal.Header closeButton>
+          <Modal.Title>Proyecto: {props.title}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Carousel>
+            {props.modalImages.map((image, index) => (
+              <Carousel.Item key={index}>
+                <img className="d-block w-100" src={image} alt={`slide-${index}`}
+                />
+              </Carousel.Item>
+            ))}
+          </Carousel>
+        </Modal.Body>
+      </Modal>
+    </>
   );
 }
+
 export default ProjectCards;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -4,31 +4,24 @@ import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import pseep from "../../Assets/Projects/pseep.png";
 import kardex from "../../Assets/Projects/kardex.png";
-import asuan from "../../Assets/Projects/asuan.png";
+import asuan from "../../Assets/Projects/gvi.png";
+import copt from "../../Assets/Projects/COPT1.jpg";
+
+// Importa las imágenes del modal
+import copt1 from "../../Assets/Projects/COPT1.jpg";
+import copt2 from "../../Assets/Projects/COPT2.jpg";
+import copt3 from "../../Assets/Projects/COPT3.jpg";
+import copt4 from "../../Assets/Projects/COPT4.jpg";
+import copt5 from "../../Assets/Projects/COPT5.jpg";
 
 function Projects({ translations }) {
   return (
     <Container fluid className="project-section">
       <Particle />
       <Container>
-        <h1 className="project-heading">
-          {translations.recentProjects}
-        </h1>
-        <p style={{ color: "white" }}>
-          {translations.projectsDescription}
-        </p>
+        <h1 className="project-heading">{translations.recentProjects}</h1>
+        <p style={{ color: "white" }}>{translations.projectsDescription}</p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={asuan}
-              isBlog={false}
-              title={translations.asuanTitle}
-              description={translations.asuanDescription}
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
-            />
-          </Col>
-
           <Col md={4} className="project-card">
             <ProjectCard
               imgPath={kardex}
@@ -36,7 +29,7 @@ function Projects({ translations }) {
               title={translations.kardexTitle}
               description={translations.kardexDescription}
               ghLink="https://github.com/Zwartmit/Kardex_SENA_CM.git"
-              demoLink="https://blogs.soumya-jit.tech/"
+              modalImages={[copt1, copt2, copt3]}
             />
           </Col>
 
@@ -47,7 +40,38 @@ function Projects({ translations }) {
               title={translations.pseepTitle}
               description={translations.pseepDescription}
               ghLink="https://github.com/Zwartmit/PSEEP_CM.git"
-              demoLink="https://editor.soumya-jit.tech/"
+              modalImages={[copt1, copt2]}
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={asuan}
+              isBlog={false}
+              title={translations.asuanTitle}
+              description={translations.asuanDescription}
+              ghLink="https://github.com/soumyajit4419/Chatify"
+              modalImages={[copt4, copt5]}
+            />
+          </Col>
+
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={copt}
+              isBlog={false}
+              title={translations.coptTitle}
+              description={[
+                translations.coptDescription1,
+                <br />,
+                translations.coptDescription2,
+                <br />,
+                translations.coptDescription3,
+                <br />,
+                translations.coptDescription4,
+                <br />,
+                translations.coptDescription5,
+              ]}
+              modalImages={[copt1, copt2, copt3, copt4, copt5]}
             />
           </Col>
         </Row>
